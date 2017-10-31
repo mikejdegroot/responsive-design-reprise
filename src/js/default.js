f110105004da3bbc0ff759f4f2f0bb03cee460bb
Mike de Groot <mike.j.degroot@gmail.com>
2017-10-31 17:10:09 +0000

    fully responsive with proper sticky scrolling implemented\!

--- src/js/default.js
+++ src/js/default.js
@@ -1,6 +1,8 @@
 // Default JavaScript Functions and Initiations
 $(document).ready(function() {
-
+  $('#fullpage').fullpage();
   // Functions go here...
-  
+  console.log('js yo');
+
+
 }); // end document ready
